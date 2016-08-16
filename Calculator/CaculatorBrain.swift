463efe764fdbc3eddfce24033d0885336c6a5367
Jason Park <jasondevelop@outlook.com>
2016-08-16 12:59:05 +0900

    Add class for calculator operation.

---
+++ Calculator/CaculatorBrain.swift
@@ -0,0 +1,22 @@
+//
+//  CaculatorBrain.swift
+//  Calculator
+//
+//  Created by Jason Park on 8/16/16.
+//  Copyright © 2016 Jason Park. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain
+{
+    func setOperand(operand: Double) { }
+    
+    func performOperation(symbol: String) { }
+    
+    var result: Double {
+        get {
+            return 0.0
+        }
+    }
+}
